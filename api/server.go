d857e2e0d2653ca1d70252d0242cf872b18ad951
chigaji <chigaji@gmail.com>
2021-06-29 23:35:12 +0900

    started handling routes

--- api/server.go
+++ api/server.go
@@ -1,16 +1,13 @@
 package api
 
 import (
+	"github.com/chigaji/myfavquotes/db"
 	"github.com/gin-gonic/gin"
 )
 
 func RunServer() {
 	r := gin.Default()
-	r.GET("/home", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Welcome home rony",
-		})
-	})
+	r.GET("/", handleHome)
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "You are home",
@@ -18,4 +15,18 @@ func RunServer() {
 	})
 	r.Run(":4000")
 
+}
+func handleHome(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "This is an app that gets your favorite quotes depending on topic of your interest",
+	})
+}
+
+func createQuote(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "This is an app that gets your favorite quotes depending on topic of your interest",
+	})
+}
+func RunDB() {
+	db.SetUpDB()
 }
