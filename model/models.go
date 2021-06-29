4514ec13e58ebf267ae904cce004a70e2ea4d6bd
ronald otwala <chigaji@gmail.com>
2021-06-29 11:56:10 +0900

    added data models

---
+++ model/models.go
@@ -0,0 +1,16 @@
+package model
+
+type FinancialQuotes struct {
+	ID    uint   `json:"id"`
+	Quote string `json:"quote"`
+}
+
+type LifeQuotes struct {
+	ID    uint   `json:"id"`
+	Quote string `json:"quote"`
+}
+
+type LoveQuotes struct {
+	ID    uint   `json:"id"`
+	Quote string `json:"quote"`
+}
