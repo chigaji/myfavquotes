721a7bd39b16e94ccafe5b50bdb92232d7464694
ronald otwala <chigaji@gmail.com>
2022-02-24 09:47:07 +0900

    changed footer text

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -6,12 +6,12 @@
     >
       <v-flex justify-center color="#2c3e50" dark text-xs-center text-sm-center text-md-center white--text>
         <span class="font-weight-bold"
-            >Bigster &copy;{{ new Date().getFullYear() }} All Rights Reserved</span>
+            >brothermen.net &copy;{{ new Date().getFullYear() }} All Rights Reserved</span>
     </v-flex>
       </v-footer>
 </template>
 <script>
-export default {}
+export default {};
 </script>
 <style lang="">
 </style>
