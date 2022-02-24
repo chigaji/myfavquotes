c0ef09fa1c57eea97b605edb2087d5e9f240540d
ronald otwala <chigaji@gmail.com>
2022-02-24 09:51:55 +0900

    added home component for home page

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,140 @@
+<template>
+  <v-container fluid>
+    <v-sheet class="main" color="white" min-height="110vh" elevation="2">
+      <v-row dense>
+        <!-- <v-container fluid> -->
+        <v-col fluid>
+          <v-card color="">
+            <v-img height="150px" src="https://picsum.photos/1920/1080?random">
+              <!-- </v-img> -->
+              <v-card-title class="text-h3 font-weight-bold white--text mt-4"
+                >This is a collection of My Favorite Quotes.
+              </v-card-title>
+            </v-img>
+            <v-card-text class="text-h5">
+              My Top Best
+              <span class="font-weight-bold blue--text">Finance</span>,
+              <span class="font-weight-bold blue--text">Life</span> and
+              <span class="font-weight-bold blue--text">Love</span> quotes.
+              <div class="ml-0 mb-2 mt-8">YOU CAN</div>
+
+              <v-timeline align-top dense>
+                <v-timeline-item
+                  v-for="message in messages"
+                  :key="message.message"
+                  :color="message.color"
+                  small
+                >
+                  <div>
+                    <div>{{ message.message }}</div>
+                  </div>
+                </v-timeline-item>
+              </v-timeline>
+            </v-card-text>
+            <!-- </v-img> -->
+          </v-card>
+        </v-col>
+        <!-- </v-container> -->
+      </v-row>
+      <v-row>
+        <!-- <v-container> -->
+        <v-col>
+          <v-card color="grey" elevation="9">
+            <v-card-title
+              class="
+                headline
+                display-1
+                font-weight-bold
+                white--text
+                text--darken-4
+              "
+              >LOVE QUOTES
+            </v-card-title>
+            <v-card-text class="text-h5 font-weight-bold">
+              <p>view love quotes</p>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn text>Enjoy</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <!-- </v-container> -->
+
+        <!-- <v-container> -->
+        <v-col>
+          <v-card color="grey" elevation="9">
+            <v-card-title
+              class="
+                headline
+                display-1
+                font-weight-bold
+                white--text
+                text--darken-4
+              "
+              >FINANCE QUOTES
+            </v-card-title>
+            <v-card-text class="text-h5 font-weight-bold">
+              <p>view financial quotes</p>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn text>Enjoy</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <!-- </v-container> -->
+        <!-- <v-container> -->
+        <v-col>
+          <v-card color="grey" elevation="9" rounded>
+            <v-card-title
+              class="
+                headline
+                display-1
+                font-weight-bold
+                white--text
+                text--darken-4
+              "
+              >LIFE QUOTES
+            </v-card-title>
+            <!-- <v-card-subtitle> </v-card-subtitle> -->
+            <v-card-text class="text-h5 font-weight-bold">
+              <p>Life Quotes</p>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn text>Enjoy</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <!-- </v-container> -->
+      </v-row>
+    </v-sheet>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      messages: [
+        {
+          message: `Create Quotes`,
+          color: "deep-purple lighten-1",
+        },
+        {
+          message: "View Quotes",
+          color: "green",
+        },
+        {
+          message: "Edit Quotes",
+          color: "deep-purple lighten-1",
+        },
+      ],
+    };
+  },
+};
+</script>
+<style lang = "sass" scoped>
+.main
+  margin-top: 0px
+  padding: 20px
+</style>
