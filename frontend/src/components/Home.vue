39a2c8e02b5068a9a9da658ba3664ac2f042d425
chigaji <chigaji@gmail.com>
2022-03-07 01:02:31 +0900

    modified homepage content

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -3,21 +3,20 @@
     <!-- <v-sheet class="main" color="white" min-height="110vh" elevation="2"> -->
     <v-row dense>
       <!-- <v-container fluid> -->
-      <v-col fluid>
-        <v-card color="">
-          <v-img height="150px" src="https://picsum.photos/1920/1080?random">
-            <!-- </v-img> -->
-            <v-card-title class="text-h3 font-weight-bold white--text mt-4"
-              >This is a collection of My Favorite Quotes.
-            </v-card-title>
-          </v-img>
-          <v-card-text class="text-h5">
-            My Top Best
+      <v-col>
+        <v-card color="#F5F5F5" elevation="1">
+          <v-card-text class="text-h6">
+            <h2 class="text-h4 black--text">
+              My <span class="green--text">Favorite</span> Quotes &
+              <span class="pink--text">Sayings</span> Collections
+            </h2>
+            <br />
+            Here are My Top Best
             <span class="font-weight-bold blue--text">Finance</span>,
             <span class="font-weight-bold blue--text">Life</span> and
             <span class="font-weight-bold blue--text">Love</span> quotes.
-            <div class="ml-0 mb-2 mt-8">YOU CAN</div>
-
+            <div class="ml-0 mb-2 mt-8">You Can:-</div>
+            <br />
             <v-timeline align-top dense>
               <v-timeline-item
                 v-for="message in messages"
@@ -31,13 +30,19 @@
               </v-timeline-item>
             </v-timeline>
           </v-card-text>
-          <!-- </v-img> -->
         </v-card>
       </v-col>
-      <!-- </v-container> -->
+      <v-col>
+        <v-img
+          src="https://picsum.photos/1920/1080?random"
+          aspect-ratio="1"
+          gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+          height="438px"
+        >
+        </v-img>
+      </v-col>
     </v-row>
     <v-row>
-      <!-- <v-container> -->
       <v-col>
         <v-card color="white" elevation="9">
           <v-img
@@ -62,7 +67,11 @@
               Random love quotes Collection
             </v-card-text>
             <v-card-actions class="justify-center">
-              <v-btn color="primary" dark>View Quotes </v-btn>
+              <v-btn large color="primary mt-8 px-10 py-5" dark
+                ><router-link to="/love" tag="span" style="cursor: pointer"
+                  >View Quotes</router-link
+                >
+              </v-btn>
             </v-card-actions>
           </v-img>
         </v-card>
@@ -117,7 +126,7 @@
               >POWER FINANCIAL QUOTES
             </v-card-title>
             <v-card-text class="text-center text-h6">
-              Random Awesome financial quotes Collection
+              Awesome financial quotes Collection
             </v-card-text>
             <v-card-actions class="justify-center">
               <v-btn color="primary" dark>View Quotes </v-btn>
@@ -125,7 +134,7 @@
           </v-img>
         </v-card>
       </v-col>
-      
+
       <!-- </v-container> -->
     </v-row>
     <!-- </v-sheet> -->
