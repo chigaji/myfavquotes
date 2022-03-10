2315d8a308b88a3c6202103640faf3d87ec081c9
ronald otwala <chigaji@gmail.com>
2022-03-10 22:14:11 +0900

    added more routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,6 +3,8 @@ import VueRouter from "vue-router";
 
 import Home from "@/components/Home";
 import Love from "@/components/Love";
+import Life from "@/components/Life";
+import Finance from "@/components/Finance";
 Vue.use(VueRouter);
 
 const routes = [
@@ -15,6 +17,16 @@ const routes = [
     path: "/love",
     name: "love",
     component: Love,
+  },
+  {
+    path: "/life",
+    name: "life",
+    component: Life,
+  },
+  {
+    path: "/finance",
+    name: "finance",
+    component: Finance,
   },
 ];
 
