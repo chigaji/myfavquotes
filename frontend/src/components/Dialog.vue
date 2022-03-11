321979cbc5350803636a6ab80d71c238aa9badf4
ronald otwala <chigaji@gmail.com>
2022-03-11 21:09:33 +0900

    template for adding quote

---
+++ frontend/src/components/Dialog.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-row justify="center">
+    <v-dialog v-model="dialog" persistent max-width="800px">
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          x-large
+          outlined
+          rounded
+          color="pink mt-12 px-10 py-5"
+          dark
+          v-bind="attrs"
+          v-on="on"
+        >
+          Add Quote
+        </v-btn>
+      </template>
+      <v-card>
+        <v-card-title>
+          <span class="text-h5">Create New Quote</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="12">
+                <v-text-field label="Enter Your Quote" required></v-text-field>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="dialog = false">
+            Close
+          </v-btn>
+          <v-btn color="blue darken-1" text @click="dialog = false">
+            Add Quote
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+<script>
+export default {
+  data: () => ({
+    dialog: false,
+  }),
+};
+</script>
